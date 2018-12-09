b58040e4a56a6d1727957502438cf8c2020ea0d6
avakrishn <avakrishn@gmail.com>
2018-12-09 00:00:48 -0800

    update functions in intswap contract

--- int-swap-dApp/contracts/IntSwap.sol
+++ int-swap-dApp/contracts/IntSwap.sol
@@ -1,90 +1,95 @@
 pragma solidity >=0.4.0 <0.6.0;
 
-import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
+import 'openzeppelin-solidity/contracts/ownership/Ownable.sol';
 
 contract IntSwap is Ownable{
     // using one-month LIBOR
-    struct OwnerOfIntSwap{
+    struct IntSwapContract{
+        uint contract_id;
         uint notional_amount;  //this is in wei
         uint escrow_amount; // we will only accept ETH for demo
         uint swap_rate; //expressed in 7 decimal points converted to integer--i.e. 0.0239539 or 2.39539% x10000000 is 239539
         uint maturity_date; //month, day, year or # of months from start timestamp
+        uint start_date; //when contract was created 
+        bool VarToFixedEscrowFlag;
+        bool FixedToVarEscrowFlag;
         address fixed_to_var_owner;
         address var_to_fixed_owner; 
     }
    //global variables 
+
+    uint256 public contract_id;
+
+    mapping (uint256 => IntSwapContract) public contract_id_to_Intswap_struct;
+
     address private OracleOwner; //should we hard code this?
-    uint public VarToFixedEscrowRecd; // amount in wei always positive integer
-    uint public FixedToVarEscrowRecd; // amount in wei always positive integer
-    uint public VarToFixedTimeRecd; // need timestamp for receipt of escrow
-    uint public FixedToVarTimeRecd; // need timestamp for receipt of escrow
-    uint public start_date; //when contract was created 
+    uint public VarToFixedEscrow; // amount in wei always positive integer
+    uint public FixedToVarEscrow; // amount in wei always positive integer
+    uint public VarToFixedTimestamp; // need timestamp for receipt of escrow
+    uint public FixedToVarTimestamp; // need timestamp for receipt of escrow
     uint public end_LIBOR; //measured at beginning of maturity month with 7 decimal places i.e. .0239539 or 2.39539%
     uint public VarToFixedPayout; //this includes return of escrow so will always be 0 or positive    
     uint public FixedToVarPayout; //this includes return of escrow so will always be 0 or positive
-    bool public VarToFixedEscrowFlag;
-    bool public FixedToVarEscrowFlag;
+ 
     
     //mapping (address => uint) public // ??do we need this??
     //enum Status { Created, Funded, Matured, PaidOut }  //use this to track status?
     //emit status
     
     function mintIntSwap ( uint _notional_amount, uint _escrow_amount, uint _swap_rate, uint _maturity_date, string _rate_type, address _counterparty) public { 
-        start_date = now;
+        contract_id++;
+        uint _start_date = now;
         // owner = msg.sender; 
-        if(_rate_type == "Fixed to Variable"){
-            fixed_to_var_owner = msg.sender;
-            var_to_fixed_owner = _counterparty;
+        address _fixed_to_var_owner;
+        address _var_to_fixed_owner;
+        if(keccak256(_rate_type) == keccak256("Fixed to Variable")){
+            _fixed_to_var_owner = msg.sender;
+            _var_to_fixed_owner = _counterparty;
         }else{
-            var_to_fixed_owner = msg.sender;
-            fixed_to_var_owner = _counterparty;
+            _var_to_fixed_owner = msg.sender;
+            _fixed_to_var_owner = _counterparty;
         }
         //we will be owner of contract unless Pavan tells us a better way.  
         //Cannot have either party be owner or they could delete it
         // use of memory on next iine below and not storage?
-       OwnerOfIntSwap memory owner_of_int_swap  = OwnerOfIntSwap({ notional_amount: _notional_amount, escrow_amount: _escrow_amount, swap_rate: _swap_rate, maturity_date: _maturity_date,  fixed_to_var_owner: _fixed_to_var_owner, var_to_fixed_owner: _var_to_fixed_owner});
+       IntSwapContract memory intswapcontract  = IntSwapContract({ contract_id: contract_id, notional_amount: _notional_amount, escrow_amount: _escrow_amount, swap_rate: _swap_rate, maturity_date: _maturity_date, start_date: _start_date,  fixed_to_var_owner: _fixed_to_var_owner, var_to_fixed_owner: _var_to_fixed_owner, VarToFixedEscrowFlag: false, FixedToVarEscrowFlag: false});
+
+       contract_id_to_Intswap_struct[contract_id] = intswapcontract;
     }
      modifier onlyVarToFixed() {
-        require(msg.sender == var_to_fixed_owner,"Only variable to fixed party can call this.");
+        require(msg.sender == contract_id_to_Intswap_struct[contract_id][var_to_fixed_owner],"Only variable to fixed party can call this function.");
         _;
     }
     modifier onlyFixedToVar() {
-        require(msg.sender == fixed_to_var_owner,"Only fixed to variable party can call this.");
+        require(msg.sender == contract_id_to_Intswap_struct[contract_id][fixed_to_var_owner],"Only fixed to variable party can call this function.");
         _;
     }
-    function receiveVarToFixedEscrow(uint amount) public payable OnlyVarToFixed returns (bool VarToFixedEscrowFlag){ 
+    function receiveVarToFixedEscrow(uint _VarToFixedEscrow) public payable onlyVarToFixed returns (bool VarToFixedEscrowFlag){ 
         // record received amount.  Can't just use true/false flag because maybe they sent wrong amount.
-        require(msg.value = amount);
-        VarToFixedEscrowRecd += amount;
-        VarToFixedTimeRecd = block.timestamp;
+        require(msg.value == _VarToFixedEscrow);
+        require(_VarToFixedEscrow == escrow_amount);
+        VarToFixedEscrow = _VarToFixedEscrow;
+        VarToFixedTimestamp = block.timestamp;
+        VarToFixedEscrowFlag = true;
         // need to test timestamp for within 48 hours -- how to do this?
         // need to send escrow back if over 48 hrs and cancel contract-not finished
         // need to send excess back if over required escrow amount
-        if (VarToFixedEscrowRecd >= escrow_amount) {VarToFixedEscrowFlag = true;
-        } 
-        else {
-        VarToFixedEscrowFlag = false;
-        }
     }
-    function receiveFixedToVarEscrow(uint amount) public payable OnlyFixedToVar returns (bool FixedToVarEscrowFlag){
-        //do we have to return two things to have access to FixedToVarEscrowRecd in later functions?
+    function receiveFixedToVarEscrow(uint _FixedToVarEscrow) public payable onlyFixedToVar returns (bool FixedToVarEscrowFlag){
+        //do we have to return two things to have access to FixedToVarEscrow in later functions?
         // receive ETH but I'm not sure how to do that--is next stmt ok?
-        require(msg.value = amount);
-        FixedToVarEscrowRecd += amount;
-        FixedToVarTimeRecd = block.timestamp;  
+        require(msg.value == _FixedToVarEscrow);
+        require (_FixedToVarEscrow == escrow_amount);
+        FixedToVarEscrow = _FixedToVarEscrow;
+        FixedToVarTimestamp = block.timestamp; 
+        FixedToVarEscrowFlag = true;
         // need to test timestamp for within 48 hours -- how to do this?
         // need to send escrow back if over 48 hrs and cancel contract--not finished 
-        // need to send excess back if over required escrow amount
-        if (FixedToVarEscrowRecd >= escrow_amount) {
-            FixedToVarEscrowFlag = true;
-        }
-        else {
-            FixedToVarEscrowFlag = false;
-        }        
+        // need to send excess back if over required escrow amount       
     }
     function OKtoProceed() public returns (bool) {
-        require (FixedToVarEscrowRecd >= escrow_amount);
-        require (VarToFixedEscrowRecd >= escrow_amount);
+        require (FixedToVarEscrow >= escrow_amount);
+        require (VarToFixedEscrow >= escrow_amount);
         //use ethereum alarm clock here to test that escrow payment period time limit (48 hrs) was met
         //expand this to test for flags at each step and notate status
         return true;
@@ -150,7 +155,7 @@ contract IntSwap is Ownable{
         return FixedToVarPayout;        
     }
        
-    function withdrawVarToFixed(uint256 amount) public OnlyVarToFixed{
+    function withdrawVarToFixed(uint256 amount) public onlyVarToFixed{
         require(VarToFixedEscrowFlag == true);
         require(msg.value = amount);
         require(amount <= VarToFixedPayout);
@@ -158,7 +163,7 @@ contract IntSwap is Ownable{
         VarToFixedPayout -= amount;
         msg.sender.transfer(amount);
     }
-    function withdrawFixedToVar(uint256 amount) public OnlyFixedToVar{
+    function withdrawFixedToVar(uint256 amount) public onlyFixedToVar{
         //can we reuse amount or does it have to have a different name because we used it in function above?
         require(FixedToVarEscrowFlag == true);
         require(msg.value = amount);
