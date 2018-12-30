ee02ba1600aea5cfa328ab4ce41839ed0c401646
M Hummer <43796202+mshummer@users.noreply.github.com>
2018-12-30 14:36:27 -0600

    Add files via upload

--- home.js
+++ home.js
@@ -3,6 +3,5 @@ $(document).ready(function(){
   $('[data-toggle="tooltip"]').tooltip(); 
   //I was trying to put #getQuote here because it is not firing when it is in bindEvents
   //it is not working here either. Any ideas?
-  $(document).on('click', '#getQuote', App.getQuote);
-
+  
 };
