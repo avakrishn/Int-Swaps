e8a9373406ae0d102b1098160ff0bb8b97017d57
avakrishn <avakrishn@gmail.com>
2019-01-07 18:19:15 -0800

    clean code

--- int-swap-dApp/src/js/app.js
+++ int-swap-dApp/src/js/app.js
@@ -1,26 +1,4 @@
 // Js file for intswap.html but name changed to getquote.html
-const rp = require('../../request-promise');
-const requestOptions = {
-  method: 'GET',
-  uri: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
-  qs: {
-    start: 1,
-    limit: 5000,
-    convert: 'USD'
-  },
-  headers: {
-    'X-CMC_PRO_API_KEY': 'a3cfb929-0767-498b-b32d-271c640e722b'
-  },
-  json: true,
-  gzip: true
-};
-
-rp(requestOptions).then(response => {
-  console.log('API call response:', response);
-}).catch((err) => {
-  console.log('API call error:', err.message);
-});
-
 
 $notional_amount = $('#notional_amount');
 $maturity_month = $('#maturity_month');
