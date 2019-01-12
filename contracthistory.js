a20647d9460ab5c5988fca96589b68b3f95d637b
avakrishn <avakrishn@gmail.com>
2019-01-11 21:41:37 -0800

    delete duplicate files

--- contracthistory.js
+++
@@ -1,2 +0,0 @@
-// Js file for contracthistory.html
-
--- home.js
+++
@@ -1,7 +0,0 @@
-// Js file for index.html and about.html
-$(document).ready(function(){
-  $('[data-toggle="tooltip"]').tooltip(); 
-  //I was trying to put #getQuote here because it is not firing when it is in bindEvents
-  //it is not working here either. Any ideas?
-  
-});
